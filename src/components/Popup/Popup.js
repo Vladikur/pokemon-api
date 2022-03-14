0265e2ee4594b0413fcbd4c3c261131843777612
Vladislav Kurmaev <Vladikurmav@yandex.ru>
2022-03-14 15:06:49 +0300

    error popup pokemon

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import pokemon from '../../images/pokemon1.png';
 
 
 function Popup({ closePopup, isOpen }) {
@@ -17,6 +18,7 @@ function Popup({ closePopup, isOpen }) {
         <button onClick={closePopup} type="button" className="popup__button-remove"/>
         <h2 className="popup__header">Опаньки...</h2>
         <p className="popup__text">При загрузке данных произошла ошибка, попробуйте перезагрузить страницу.</p>
+        <img src={pokemon} alt="Покемон" className='popup__image'></img>
         <button onClick={pageReload} className="popup__button-reload" type="button">Перезагрузка!</button>
       </div>
     </section>
